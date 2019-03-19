6b9effca2e201529bb02e22d0a4f1904885f139a
sotirelisc <gtakriz@hotmail.com>
2019-03-19 20:22:24 +0200

    Wire up Redux action to select which section to expand.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <Text>
+          {library.description}
+        </Text>
+      );
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
+    const { id, title } = this.props.library;
 
     return (
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -23,4 +45,13 @@ const styles = {
   }
 };
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+  // Move expansion logic out of component
+  const expanded = state.selectedLibraryId === ownProps.libraryId;
+
+  return {
+    expanded
+  };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
